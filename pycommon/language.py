0be7d59f772d7cb851b60c9e5872e1cd364e2861
Walter Moreira <moreira@astro.as.utexas.edu>
2010-09-13 16:19:39 -0500

    Basic client can list messages

--- pycommon/language.py
+++ pycommon/language.py
@@ -7,6 +7,10 @@ PUNCTUATION = maketrans(punctuation, ' '*len(punctuation))
 def stem(word):
     return stemming.porter2.stem(word)
 
+def stem_words(words):
+    # TODO: see if we need the original capitalization
+    return [stem(word.lower()) for word in words]
+
 def stem_phrase(phrase):
     words = phrase.translate(PUNCTUATION).split()
-    return [stem(word) for word in words]
+    return stem_words(words)
