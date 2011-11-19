e9160010817c8d0f82edb71af0bdc715d8f1e568
Walter Moreira <moreira@astro.as.utexas.edu>
2011-11-19 17:19:46 -0600

    Support more than PIPE_BUF character in msgs
    If the messages are big, we need to split them, keep identification of clients, and concatenate on read.

--- pycommon/actors/manager.py
+++ pycommon/actors/manager.py
@@ -3,6 +3,10 @@ from gevent.server import StreamServer
 from gevent.queue import Queue, Empty
 from gevent.coros import RLock
 from textwrap import dedent
+from itertools import izip_longest, imap
+import struct
+import select
+import uuid
 import traceback
 import multiprocessing
 import json
@@ -16,47 +20,116 @@ MANAGER_ADDRESS = 'manager'
 
 logger = log.setup('manager', 'to_console')
 
+def write(p, c):
+    p.write(c*100000)
+    print 'wrote', c, 'to', p
+    
+def test():
+    p1 = Pipe('foo', 'r')
+    p2 = Pipe('foo', 'w')
+    p3 = Pipe('foo', 'w')
+
+    import threading
+    t2 = threading.Thread(target=write, args=(p2, 'X'))
+    #t3 = threading.Thread(target=write, args=(p3, '_'))
+    t2.start()
+    #t3.start()
+    t2.join()
+    #t3.join()
+    return p1
+
+def grouper(n, iterable):
+    "grouper(3, 'ABCDEFG') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return imap(lambda x: ''.join(x),
+                izip_longest(fillvalue=' ', *args))
+
 class Pipe(object):
 
-    def __init__(self, name):
+    MAX_BUFSIZE = 10
+    MAX_UNSPLIT = 10
+    
+    def __init__(self, name, mode='r'):
+        self.parts = {}
         self.path = self._path_to(name)
         if not os.path.exists(self.path):
             os.mkfifo(self.path)
-        self.fd = None
-        self._aux_fd = None
-        self.open_for = None
+        self.mode = mode
+        if mode == 'r':
+            self._open_read_pipe()
+        elif mode == 'w':
+            self._open_write_pipe()
+        else:
+            raise Exception("unknown mode. Should be 'r' or 'w'")
 
     def close(self):
-        if self.fd is not None:
-            self.fd.close()
-            self._aux_fd.close()
-            self.fd = self._aux_fd = None
-            self.open_for = None
+        """
+        Close this end of the pipe. The same object can be used
+        again, for reading or writing.
+        """
+        self.fd.close()
+        self._aux_fd.close()
 
     def destroy(self):
+        """
+        Destroy the pipe, by removing the pipe.
+        """
         self.close()
         os.unlink(self.path)
-        
-    def write(self, data):
-        if self.open_for == 'read':
-            raise Exception('pipe already open for reading')
-        if self.open_for is None:
-            self._open_write_pipe()
-        self.fd.write(json.dumps(data)+'\n')
 
+    def _pack(self, ident, n_part, s):
+        return struct.pack('c32si{}sc'.format(self.MAX_BUFSIZE),
+                           '}', ident.hex, n_part, s, '\n')
+
+    def _unpack(self, s):
+        return struct.unpack('c32si{}sc'.format(self.MAX_BUFSIZE), s)
+                             
+    def _split_write(self, s):
+        ident = uuid.uuid1()
+        for i, part in enumerate(grouper(self.MAX_BUFSIZE, s)):
+            self.fd.write(self._pack(ident, i, part))
+        self.fd.write(self._pack(ident, -1, ''))
+                           
+    def write(self, data):
+        if self.mode == 'r':
+            raise Exception('pipe already open for reading')
+        buf = json.dumps(data)
+        if len(buf) < self.MAX_UNSPLIT:
+            self.fd.write(buf+'\n')
+        else:
+            self._split_write(buf)
+
+    def _split_read(self, s):
+        _, ident, part, data, _ = self._unpack(s)
+        if part < 0:
+            # we have all the data, return it
+            result = []
+            parts = self.parts[ident]
+            for k in range(len(parts)):
+                result.append(parts[k])
+            del self.parts[ident]
+            return json.loads(''.join(result))
+        parts = self.parts.setdefault(ident, {})
+        parts[part] = data
+        return None
+        
     def read(self):
-        if self.open_for == 'write':
+        if self.mode == 'w':
             raise Exception('pipe already open for writing')
-        if self.open_for is None:
-            self._open_read_pipe()
         try:
-            return json.loads(self.fd.readline())
+            data = self.fd.readline()
+            if data[0] == '}':
+                # cannot be a JSON, so we are reading split big chunk of data
+                return self._split_read(data)
+            else:
+                return json.loads(data)
         except IOError as err:
             if err.errno == errno.EWOULDBLOCK:
                 # No data from clients
                 return None
             raise
         except ValueError:
+            traceback.print_exc()
             # Got wrong data, or EOF
             return None
 
@@ -67,7 +140,6 @@ class Pipe(object):
             os.open(self.path, os.O_RDONLY | os.O_NONBLOCK), 'r', 0)
         self.fd = os.fdopen(
             os.open(self.path, os.O_WRONLY | os.O_NONBLOCK), 'w', 0)
-        self.open_for = 'write'
 
     def _open_read_pipe(self):
         self.fd = os.fdopen(
@@ -76,7 +148,6 @@ class Pipe(object):
         # clients disconnect
         self._aux_fd = os.fdopen(
             os.open(self.path, os.O_WRONLY | os.O_NONBLOCK), 'w', 0)
-        self.open_for = 'read'
 
     def _path_to(self, name):
         return os.path.join('/tmp/actor_pipes', name)
