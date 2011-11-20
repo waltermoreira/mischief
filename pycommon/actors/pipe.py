b7a724d8d442a3f27a38bcff0dbe3f0ffa2381ac
Walter Moreira <moreira@astro.as.utexas.edu>
2011-11-19 23:25:00 -0600

    Rearranged code and added comments

--- pycommon/actors/pipe.py
+++ pycommon/actors/pipe.py
@@ -8,38 +8,29 @@ import errno
 import os
 import threading
 
-def write(p, c):
-    p.write(c*100000)
-    print 'ended writing', c
-    
-def test():
-    p1 = Pipe('foo', 'r')
-
-    symbols = ['X', '_', 'R', '$']
-    threads = []
-    pipes = []
-    for c in symbols:
-        p = Pipe('foo', 'w')
-        pipes.append(p)
-        threads.append(threading.Thread(target=write, args=(p, c)))
-    for t in threads:
-        t.start()
-    
-    return p1
-
-def grouper(n, iterable):
-    "grouper(3, 'ABCDEFG') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return imap(lambda x: ''.join(x),
-                izip_longest(fillvalue=' ', *args))
-
 class Pipe(object):
 
-    # Leave room to add identifier and part number
-    MAX_BUFSIZE = select.PIPE_BUF - 50
-    # substract 1 to account for the additional \n
+    # Maximum size allowed for the json string representing the data,
+    # before splitting it in a multi-part message.
+    #
+    # By default, we want it to fit in PIPE_BUF bytes, as the write
+    # operation is guarantee to be atomic (substract 1 to account for
+    # the additional \n)
     MAX_UNSPLIT = select.PIPE_BUF - 1
 
+    # Size of chunks into which we split the json data when it is
+    # bigger than PIPE_BUF.
+    #
+    # Leave room to add identifier and part number
+    MAX_BUFSIZE = select.PIPE_BUF - 50
+
+    # Format to serialize the chunks of json data.
+    #
+    # '}' + 32 char ident + json_chunk
+    #
+    # The initial '}' is so we can tell a split message from a json
+    # message, since a string starting with '}' is not a valid json
+    # object.
     STRUCT = 'c32si{}s'.format(MAX_BUFSIZE)
     
     def __init__(self, name, mode='r'):
@@ -78,9 +69,17 @@ class Pipe(object):
         return struct.unpack(self.STRUCT, s)
                              
     def _split_write(self, s):
+        """
+        Split the string 's' into parts that fit in PIPE_BUF, and
+        write them to the pipe.
+
+        Careful: if the string 's' is bigger than the full pipe
+        buffers (usually 64k), this write will block until there is
+        space in the pipe to write all the remaining parts.
+        """
         ident = uuid.uuid1()
         for i, part in enumerate(grouper(self.MAX_BUFSIZE, s)):
-            print 'writing part', i
+            # write a part
             self.fd.write(self._pack(ident, i, part))
         # write part number -1 to signal end of parts
         self.fd.write(self._pack(ident, -1, ' '*self.MAX_BUFSIZE))
@@ -90,14 +89,16 @@ class Pipe(object):
             raise Exception('pipe already open for reading')
         buf = json.dumps(data)
         if len(buf) < self.MAX_UNSPLIT:
+            # if the json string fits in PIPE_BUF bytes, just write it
+            # with a newline, so it can be read with 'readline'
             self.fd.write(buf+'\n')
         else:
+            # if the json string is too big, split it
             self._split_write(buf)
 
     def _split_read(self, s):
         _, ident, part, data = self._unpack(s)
         if part < 0:
-            print 'collecting parts'
             # we have all the data, concatenate it in the proper order
             parts = self.parts[ident]
             result = [parts[k] for k in range(len(parts))]
@@ -108,7 +109,8 @@ class Pipe(object):
         # save the data with the proper part number
         parts = self.parts.setdefault(ident, {})
         parts[part] = data
-        print 'saved part'
+        # Return as if there were no data in the pipe, until all parts
+        # are read and concatenated
         return None
         
     def _read(self):
@@ -117,18 +119,19 @@ class Pipe(object):
         try:
             first_char = self.fd.read(1)
             if first_char == '}':
-                print 'multipart message'
-                # cannot be a JSON, so we are reading split big chunk of data
+                # cannot be a JSON, so we are reading a split big
+                # chunk of data
                 data = first_char + self.fd.read(struct.calcsize(self.STRUCT)-1)
                 return self._split_read(data)
             else:
-                print 'json data'
+                # read a full line, and decode it as json
                 data = first_char + self.fd.readline()
                 return json.loads(data)
         except IOError as err:
             if err.errno == errno.EWOULDBLOCK:
                 # No data from clients
                 return None
+            # do not ignore other kind of ioerrors
             raise
         except ValueError:
             traceback.print_exc()
@@ -137,12 +140,20 @@ class Pipe(object):
 
     def read(self, block=False):
         if block:
+            # Try to read data until we get an object. If there is
+            # data and it is a simple json object, it will return
+            # immediatly. Otherwise, it will block and keep waiting or
+            # reading parts of a multi-part message until it is
+            # complete
             while True:
                 data = self._read()
                 if data is not None:
                     return data
                 time.sleep(0.01)
         else:
+            # Return an object or None (in case there is no data in
+            # the pipe or there is an still incomplete multi-part
+            # message)
             return self._read()
                 
     def _open_write_pipe(self):
@@ -163,5 +174,30 @@ class Pipe(object):
 
     def _path_to(self, name):
         return os.path.join('/tmp/actor_pipes', name)
+
+def grouper(n, iterable):
+    """
+    grouper(3, 'ABCDEFG') --> ABC DEF Gxx
+    """
+    args = [iter(iterable)] * n
+    return imap(lambda x: ''.join(x),
+                izip_longest(fillvalue=' ', *args))
+
+def write(p, c):
+    p.write(c*100000)
+    print 'ended writing', c
     
-    
+def test():
+    p1 = Pipe('foo', 'r')
+
+    symbols = ['X', '_', 'R', '$']
+    threads = []
+    pipes = []
+    for c in symbols:
+        p = Pipe('foo', 'w')
+        pipes.append(p)
+        threads.append(threading.Thread(target=write, args=(p, c)))
+    for t in threads:
+        t.start()
+    return p1
+
