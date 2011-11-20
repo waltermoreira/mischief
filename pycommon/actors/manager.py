d68c4584b7d0d78c39100cbb33388b7689280156
Walter Moreira <moreira@astro.as.utexas.edu>
2011-11-19 19:49:21 -0600

    Support msgs bigger than full pipe buffer (64k)

--- pycommon/actors/manager.py
+++ pycommon/actors/manager.py
@@ -30,11 +30,12 @@ def test():
     p3 = Pipe('foo', 'w')
 
     import threading
-    t2 = threading.Thread(target=write, args=(p2, 'X'))
-    #t3 = threading.Thread(target=write, args=(p3, '_'))
-    t2.start()
-    #t3.start()
-    t2.join()
+    write(p2, 'X')
+    # t2 = threading.Thread(target=write, args=(p2, 'X'))
+    # #t3 = threading.Thread(target=write, args=(p3, '_'))
+    # t2.start()
+    # #t3.start()
+    # t2.join()
     #t3.join()
     return p1
 
@@ -46,8 +47,12 @@ def grouper(n, iterable):
 
 class Pipe(object):
 
-    MAX_BUFSIZE = 10
-    MAX_UNSPLIT = 10
+    # Leave room to add identifier and part number
+    MAX_BUFSIZE = select.PIPE_BUF - 50
+    # substract 1 to account for the additional \n
+    MAX_UNSPLIT = select.PIPE_BUF - 1
+
+    STRUCT = 'c32si{}s'.format(MAX_BUFSIZE)
     
     def __init__(self, name, mode='r'):
         self.parts = {}
@@ -78,17 +83,20 @@ class Pipe(object):
         os.unlink(self.path)
 
     def _pack(self, ident, n_part, s):
-        return struct.pack('c32si{}sc'.format(self.MAX_BUFSIZE),
-                           '}', ident.hex, n_part, s, '\n')
+        return struct.pack(self.STRUCT,
+                           '}', ident.hex, n_part, s)
 
     def _unpack(self, s):
-        return struct.unpack('c32si{}sc'.format(self.MAX_BUFSIZE), s)
+        return struct.unpack(self.STRUCT, s)
                              
     def _split_write(self, s):
         ident = uuid.uuid1()
         for i, part in enumerate(grouper(self.MAX_BUFSIZE, s)):
+            print 'writing part', i
+            print '  part', part
             self.fd.write(self._pack(ident, i, part))
-        self.fd.write(self._pack(ident, -1, ''))
+        # write part number -1 to signal end of parts
+        self.fd.write(self._pack(ident, -1, ' '*self.MAX_BUFSIZE))
                            
     def write(self, data):
         if self.mode == 'r':
@@ -100,15 +108,20 @@ class Pipe(object):
             self._split_write(buf)
 
     def _split_read(self, s):
-        _, ident, part, data, _ = self._unpack(s)
+        _, ident, part, data = self._unpack(s)
         if part < 0:
-            # we have all the data, return it
+            # we have all the data, concatenate it in the proper order
             result = []
+            print 'we got all the parts!'
+            print self.parts[ident]
             parts = self.parts[ident]
             for k in range(len(parts)):
                 result.append(parts[k])
+            # forget all parts for this client
             del self.parts[ident]
+            # return the JSON
             return json.loads(''.join(result))
+        # save the data with the proper part number
         parts = self.parts.setdefault(ident, {})
         parts[part] = data
         return None
@@ -117,11 +130,13 @@ class Pipe(object):
         if self.mode == 'w':
             raise Exception('pipe already open for writing')
         try:
-            data = self.fd.readline()
-            if data[0] == '}':
+            first_char = self.fd.read(1)
+            if first_char == '}':
                 # cannot be a JSON, so we are reading split big chunk of data
+                data = first_char + self.fd.read(struct.calcsize(self.STRUCT)-1)
                 return self._split_read(data)
             else:
+                data = first_char + self.fd.readline()
                 return json.loads(data)
         except IOError as err:
             if err.errno == errno.EWOULDBLOCK:
@@ -139,7 +154,7 @@ class Pipe(object):
         self._aux_fd = os.fdopen(
             os.open(self.path, os.O_RDONLY | os.O_NONBLOCK), 'r', 0)
         self.fd = os.fdopen(
-            os.open(self.path, os.O_WRONLY | os.O_NONBLOCK), 'w', 0)
+            os.open(self.path, os.O_WRONLY), 'w', 0)
 
     def _open_read_pipe(self):
         self.fd = os.fdopen(
