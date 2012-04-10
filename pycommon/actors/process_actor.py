79c419a1e682f288c04d5db2c0156d5a8b7f43a4
Walter Moreira <walter@waltermoreira.net>
2012-04-10 15:39:51 -0500

    Commented the process-based actor class

--- pycommon/actors/process_actor.py
+++ pycommon/actors/process_actor.py
@@ -1,3 +1,23 @@
+"""
+A base class to construct process-based actors
+==============================================
+
+Inherit from the class ``ProcessActor`` to create an actor which will
+run in an independent process.
+
+Use as::
+
+    from pycommon.actors.process_actor import ProcessActor
+
+    class MyActor(ProcessActor):
+
+        def __init__(self):
+            super(MyActor, self).__init__('MyActorName')
+
+        ...
+
+"""
+
 from pycommon.actors.actor import Actor, ActorRef
 import importlib
 import os
@@ -6,6 +26,9 @@ import subprocess
 
 class ProcessActor(Actor):
 
+    # When creating an instance, the first time launch a new Python
+    # process. Subsequent times (from inside the new process) just
+    # create a regular instance.
     launch = True
     
     def __init__(self, *args, **kwargs):
@@ -15,7 +38,11 @@ class ProcessActor(Actor):
                         self.__class__.__module__)
 
 class WaitActor(Actor):
-
+    """
+    Actor to wait for a message from the client's class saying that
+    it started ok.
+    """
+    
     def __init__(self):
         super(WaitActor, self).__init__()
         
@@ -23,6 +50,13 @@ class WaitActor(Actor):
         self.receive({'ok': lambda msg: None})
      
 def start_actor(name, module):
+    """
+    Start a new Python subprocess.
+
+    We pass the information of the client's object to instantiate, and
+    the waiting actor to confirm that everything went well on
+    startup.
+    """
     myself = os.path.abspath(__file__)
     if myself.endswith('.pyc'):
         myself = myself[:-1]
@@ -39,16 +73,19 @@ if __name__ == '__main__':
     actor_module = sys.argv[3]
     mod = importlib.import_module(actor_module)
     cls = getattr(mod, actor_class)
+    # Signal the base class ``ProcessActor`` to not start a new
+    # subprocess (we are already in it!)
     cls.launch = False
     actor = cls()
-    print 'actor created', actor
     
+    # Tell parent to keep going
     wait.send({'tag': 'ok'})
     wait.destroy_ref()
 
-    print 'ok sent, acting...'
+    # The new process ends when the client's actor finishes its
+    # ``act`` method.
     actor.act()
-    print 'act finished'
+
     actor_ref = ActorRef(actor.name)
     actor_ref.destroy_actor()
     
