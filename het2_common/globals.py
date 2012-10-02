09028128fc07fdac34b6a079dd0f03672efeba40
Walter Moreira <walter@waltermoreira.net>
2012-10-02 13:51:06 -0500

    Global constants to convert units

--- het2_common/globals.py
+++ het2_common/globals.py
@@ -2,3 +2,11 @@ from os.path import dirname, abspath, join
 
 cwd = dirname(abspath(__file__))
 HET2_DEPLOY = abspath(join(cwd, '../../../'))
+
+# degrees to radians
+DEGREES = math.pi/180.0
+# radians to degrees
+RADIANS = 180.0/math.pi
+
+# seconds to mjd
+SECONDS = 1.0/(24*60*60)
