ee4919a3b052b15888b25c85506c64e037940f21
Walter Moreira <moreira@astro.as.utexas.edu>
2012-10-22 00:15:37 -0500

    Improved proc_actor for testing with new pipes

--- test/proc_actor.py
+++ test/proc_actor.py
@@ -1,47 +1,28 @@
-from het2_common.actors.actor import Actor, ActorRef
+from het2_common.actors.actor import ActorRef
+from het2_common.actors.process_actor import ProcessActor
 import subprocess
 import os.path
 import sys
 import time
 
-def process_actor():
-    myself = os.path.abspath(__file__)
-    if myself.endswith('.pyc'):
-        myself = myself[:-1]
-    p = subprocess.Popen(['python', myself])
-    time.sleep(2)
+class _process_actor(ProcessActor):
 
-
-class _process_actor(Actor):
-
+    def __init__(self):
+        super(_process_actor, self).__init__('p')
+        
     def reply(self, msg):
-        sender = ActorRef(msg['reply_to'])
-        sender.send({'tag': 'answer',
-                     'answer': 5})
+        with ActorRef(msg['reply_to']) as sender:
+            sender.answer(answer=5)
 
     def queue(self, msg):
-        sender = ActorRef(msg['reply_to'])
-        sender.send({'tag': 'answer',
-                     'answer': 2})
-
-    def spawn(self, msg):
-        _child('child')
-        ActorRef(msg['reply_to']).send({
-            'tag': 'reply'
-            })
+        with ActorRef(msg['reply_to']) as sender:
+            sender.answer(answer=2)
 
     def quit(self, msg):
         sys.exit(0)
         
-    def act(self):
-        while True:
-            self.receive({
-                'reply': 'reply',
-                'queue': 'queue',
-                'spawn': 'spawn',
-                'quit': 'quit'
-            })
-
-if __name__ == '__main__':
-    q = _process_actor('p')
-    q.act()
+    def process_act(self):
+        self.receive(
+            reply = self.reply,
+            queue = self.queue,
+            quit = self.quit)
