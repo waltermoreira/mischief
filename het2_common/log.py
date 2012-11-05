7fa148de907faaab5c42ff05a229509866ed8ce6
Walter Moreira <walter@waltermoreira.net>
2012-11-05 15:46:24 -0600

    Remove separator in logging
    It doesn't help as much as distracts when going to console.

--- het2_common/log.py
+++ het2_common/log.py
@@ -135,7 +135,6 @@ def setup(mod_name, to_where='to_everywhere'):
                     'to_logger': 'logger',
                     'to_everywhere': 'full'}
     logger = logging.getLogger(mod_name+'.'+destinations[to_where])
-    logger.debug('--------------------')
     return logger
     
 def test(s):
