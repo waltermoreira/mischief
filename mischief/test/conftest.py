ba5144a0ee0e632ffe2fb2b39191f588fce0f399
Walter Moreira <walter@waltermoreira.net>
2013-04-12 14:30:39 -0500

    Rearranging tests

--- mischief/test/conftest.py
+++ mischief/test/conftest.py
@@ -1,72 +1,89 @@
-from het2_common.actors.actor import ThreadedActor, ActorRef, Actor
-from het2_common.actors.process_actor import spawn
-import multiprocessing
-import signal
-import time
+import sys
 import os
-from proc_actor import _process_actor, _with_name, _with_name_2
 
-# def pytest_funcarg__qm(request):
-#     return request.cached_setup(
-#         setup=create_queue_manager,
-#         teardown=lambda x: destroy(),
-#         scope='session')
+# Append directory where the current package
+sys.path.append(
+    os.path.join('../../', os.path.dirname(os.path.abspath(__file__))))
 
-# def destroy():
-#     ActorRef('t').destroy_actor()
-#     ActorRef('p').send({'tag': 'quit'})
-#     ActorRef('p').destroy_actor()
+from mischief.actors.actor import Actor, ActorRef, ThreadedActor
+from mischief.actors.pipe import NameBroker
+
+# from mischief.actors.actor import ThreadedActor, ActorRef, Actor
+# #from mischief.actors.process_actor import spawn
+# import multiprocessing
+# import signal
+# import time
+# import os
+# from proc_actor import _process_actor, _with_name, _with_name_2
+
+# # def pytest_funcarg__qm(request):
+# #     return request.cached_setup(
+# #         setup=create_queue_manager,
+# #         teardown=lambda x: destroy(),
+# #         scope='session')
+
+# # def destroy():
+# #     ActorRef('t').destroy_actor()
+# #     ActorRef('p').send({'tag': 'quit'})
+# #     ActorRef('p').destroy_actor()
 
 class _threaded_actor(ThreadedActor):
 
-    def reply(self, msg):
+    def reply5(self, msg):
         sender = ActorRef(msg['reply_to'])
-        sender.send({'tag': 'answer',
-                     'answer': 5})
+        sender.answer(answer=5)
 
-    def queue(self, msg):
+    def reply2(self, msg):
         sender = ActorRef(msg['reply_to'])
-        sender.send({'tag': 'answer',
-                     'answer': 2})
+        sender.answer(answer=2)
 
     def act(self):
-        q = []
-        while not q:
-            self.receive({
-                'reply': 'reply',
-                'queue': 'queue',
-                'stop': lambda msg: q.append(1)})
-
-
-# def create_queue_manager():
-#     t = _threaded_actor('t')
-#     process_actor()
-#     p = _process_actor('p')
+        while True:
+            self.receive(
+                reply5 = self.reply5,
+                reply2 = self.reply2)
 
 def pytest_funcarg__t(request):
     return request.cached_setup(
-        setup=create_threaded_actor,
+        setup=lambda: _threaded_actor(name='t'),
         teardown=lambda x: x.close(),
         scope='session')
 
-def create_threaded_actor():
-    return _threaded_actor('t')
-
-def pytest_funcarg__p(request):
+def start_namebroker():
+    x = NameBroker()
+    x.start()
+    return x
+    
+def pytest_funcarg__nb(request):
     return request.cached_setup(
-        setup=create_process_actor,
-        teardown=lambda x: x.close_actor(),
+        setup=start_namebroker,
+        teardown=lambda x: x.stop(),
         scope='session')
+    
+# # def create_queue_manager():
+# #     t = _threaded_actor('t')
+# #     process_actor()
+# #     p = _process_actor('p')
 
-def create_process_actor():
-    spawn(_process_actor, 'p')
-    ref = ActorRef('p')
-    while not ref.is_alive():
-        time.sleep(0.1)
-    return ref
 
-def pytest_funcarg__q(request):
-    return _with_name
+# def create_threaded_actor():
+#     return _threaded_actor('t')
 
-def pytest_funcarg__q2(request):
-    return _with_name_2
+# def pytest_funcarg__p(request):
+#     return request.cached_setup(
+#         setup=create_process_actor,
+#         teardown=lambda x: x.close_actor(),
+#         scope='session')
+
+# def create_process_actor():
+#     spawn(_process_actor, 'p')
+#     ref = ActorRef('p')
+#     while not ref.is_alive():
+#         time.sleep(0.1)
+#     return ref
+
+# def pytest_funcarg__q(request):
+#     return _with_name
+
+# def pytest_funcarg__q2(request):
+#     return _with_name_2
--- mischief/test/test_actor.py
+++ mischief/test/test_actor.py
@@ -1,369 +1,368 @@
-from het2_common.actors.actor import Actor, ActorRef, ThreadedActor, ActorFinished
-from het2_common.actors.process_actor import spawn
-from het2_common.globals import DEPLOY_PATH
+
+from mischief.actors.actor import Actor, ActorRef, ThreadedActor, ActorFinished
+#from het2_common.actors.process_actor import spawn
+#from het2_common.globals import DEPLOY_PATH
 import time
 import py
 import os
 
-# Add path of tests to PYTHONPATH, since ProcessActor need to be in
-# the path
-os.environ['PYTHONPATH'] = ':'.join([os.environ['PYTHONPATH'],
-                                     os.path.join(DEPLOY_PATH, 'test')])
+# # Add path of tests to PYTHONPATH, since ProcessActor need to be in
+# # the path
+# os.environ['PYTHONPATH'] = ':'.join([os.environ['PYTHONPATH'],
+#                                      os.path.join(DEPLOY_PATH, 'test')])
 
-def test_reply(t):
+def test_reply(nb, t):
     class a(Actor):
         def act(self):
             result = []
             self.receive(
                 answer = lambda msg: result.append(msg['answer']))
             return result[0]
-    x = a()
-    qt = ActorRef('t')
-    qt.reply(reply_to=x.name)
-    assert x.act() == 5
-    x.close()
+    with a() as x, ActorRef(t.address()) as qt:
+        qt.reply5(reply_to=x)
+        assert x.act() == 5
 
-def test_inbox(t):
-    class a(Actor):
-        def act(self):
-            result = []
-            self.receive({
-                'answer': lambda msg: result.append(msg['answer'])})
-            return result[0]
-    x = a()
-    qt = ActorRef('t')
-    qt.foo()
-    qt.bar()
-    qt.queue(reply_to=x.name)
-    assert x.act() == 2
-    x.close()
+# def test_inbox(t):
+#     class a(Actor):
+#         def act(self):
+#             result = []
+#             self.receive({
+#                 'answer': lambda msg: result.append(msg['answer'])})
+#             return result[0]
+#     x = a()
+#     qt = ActorRef('t')
+#     qt.foo()
+#     qt.bar()
+#     qt.queue(reply_to=x.name)
+#     assert x.act() == 2
+#     x.close()
 
-def test_wildcard(t):
-    class a(Actor):
-        def act(self):
-            result = []
-            self.receive(
-                _ = lambda msg: result.append(msg['tag']))
-            return result
-    x = a()
-    qa = ActorRef(x.name)
-    qa.foo()
-    assert x.act() == ['foo']
-    x.close()
+# def test_wildcard(t):
+#     class a(Actor):
+#         def act(self):
+#             result = []
+#             self.receive(
+#                 _ = lambda msg: result.append(msg['tag']))
+#             return result
+#     x = a()
+#     qa = ActorRef(x.name)
+#     qa.foo()
+#     assert x.act() == ['foo']
+#     x.close()
 
-def test_timeout(t):
-    class a(Actor):
-        def act(self):
-            result = [False]
-            self.receive(
-                timed_out = lambda msg: result.append(True),
-                timeout = 0.1)
-            return result
-    x = a()
-    assert x.act()[-1]
-    x.close()
+# def test_timeout(t):
+#     class a(Actor):
+#         def act(self):
+#             result = [False]
+#             self.receive(
+#                 timed_out = lambda msg: result.append(True),
+#                 timeout = 0.1)
+#             return result
+#     x = a()
+#     assert x.act()[-1]
+#     x.close()
 
-def test_read_value(t):
-    class a(Actor):
-        def act(self):
-            self.receive(
-                ack = self.read_value('foo'))
-            return self.foo
-    x = a()
-    qm = ActorRef(x.name)
-    qm.ack(foo=5)
-    assert x.act() == 5
-    x.close()
+# def test_read_value(t):
+#     class a(Actor):
+#         def act(self):
+#             self.receive(
+#                 ack = self.read_value('foo'))
+#             return self.foo
+#     x = a()
+#     qm = ActorRef(x.name)
+#     qm.ack(foo=5)
+#     assert x.act() == 5
+#     x.close()
 
 
-def test_new_api(t):
-    class a(Actor):
-        def act(self):
-            self.receive({'foo': self.foo})
-            return self._msg
-        def foo(self, msg):
-            self._msg = msg
-    x = a()
-    qx = ActorRef(x.name)
-    qx.foo(bar=5, baz='baz')
-    msg = x.act()
-    assert (msg['tag'] == 'foo' and
-            msg['bar'] == 5 and
-            msg['baz'] == 'baz')
-    x.close()
+# def test_new_api(t):
+#     class a(Actor):
+#         def act(self):
+#             self.receive({'foo': self.foo})
+#             return self._msg
+#         def foo(self, msg):
+#             self._msg = msg
+#     x = a()
+#     qx = ActorRef(x.name)
+#     qx.foo(bar=5, baz='baz')
+#     msg = x.act()
+#     assert (msg['tag'] == 'foo' and
+#             msg['bar'] == 5 and
+#             msg['baz'] == 'baz')
+#     x.close()
 
-def test_new_api_2(t):
-    class a(Actor):
-        pass
-    x = a()
-    qx = ActorRef(x.name)
-    with py.test.raises(TypeError):
-        qx()
-    qx.foo()
-    with py.test.raises(TypeError):
-        qx()
-    x.close()
+# def test_new_api_2(t):
+#     class a(Actor):
+#         pass
+#     x = a()
+#     qx = ActorRef(x.name)
+#     with py.test.raises(TypeError):
+#         qx()
+#     qx.foo()
+#     with py.test.raises(TypeError):
+#         qx()
+#     x.close()
 
-def test_many_msgs(t):
-    # Send many msgs to one actor, collect them and count them
-    class a(ThreadedActor):
-        def __init__(self):
-            super(a, self).__init__()
-            self.results = {}
-        def act(self):
-            try:
-                while True:
-                    self.receive(add=self.add)
-            except ActorFinished:
-                pass
-        def add(self, msg):
-            self.results[msg['i']] = 1
-            if sorted(self.results.keys()) == range(100):
-                with ActorRef(msg['reply_to']) as sender:
-                    sender.got_all()
-    class b(Actor):
-        def act(self):
-            result = []
-            self.receive(got_all=lambda msg: result.append(True),
-                         timed_out=lambda msg: result.append(False),
-                         timeout=2)
-            return result
-    # Create many instances to check the pipes are refreshed for each instance
-    actors = [a() for i in range(4)]
-    y = b()
-    x_ref = ActorRef(actors[-1].name)
-    for i in range(100):
-        x_ref.add(i=i, reply_to=y.name)
-    res = y.act()
-    assert res == [True]
-    [x.close() for x in actors]
-    y.close()
+# def test_many_msgs(t):
+#     # Send many msgs to one actor, collect them and count them
+#     class a(ThreadedActor):
+#         def __init__(self):
+#             super(a, self).__init__()
+#             self.results = {}
+#         def act(self):
+#             try:
+#                 while True:
+#                     self.receive(add=self.add)
+#             except ActorFinished:
+#                 pass
+#         def add(self, msg):
+#             self.results[msg['i']] = 1
+#             if sorted(self.results.keys()) == range(100):
+#                 with ActorRef(msg['reply_to']) as sender:
+#                     sender.got_all()
+#     class b(Actor):
+#         def act(self):
+#             result = []
+#             self.receive(got_all=lambda msg: result.append(True),
+#                          timed_out=lambda msg: result.append(False),
+#                          timeout=2)
+#             return result
+#     # Create many instances to check the pipes are refreshed for each instance
+#     actors = [a() for i in range(4)]
+#     y = b()
+#     x_ref = ActorRef(actors[-1].name)
+#     for i in range(100):
+#         x_ref.add(i=i, reply_to=y.name)
+#     res = y.act()
+#     assert res == [True]
+#     [x.close() for x in actors]
+#     y.close()
 
-def test_reply(p):
-    class a(Actor):
-        def act(self):
-            result = []
-            self.receive({
-                'answer': lambda msg: result.append(msg['answer'])})
-            return result[0]
-    x = a('a')
-    qt = ActorRef('p')
-    qt.send({'tag': 'reply',
-             'reply_to': 'a'})
-    assert x.act() == 5
-    x.close()
+# def test_reply(p):
+#     class a(Actor):
+#         def act(self):
+#             result = []
+#             self.receive({
+#                 'answer': lambda msg: result.append(msg['answer'])})
+#             return result[0]
+#     x = a('a')
+#     qt = ActorRef('p')
+#     qt.send({'tag': 'reply',
+#              'reply_to': 'a'})
+#     assert x.act() == 5
+#     x.close()
     
 
-def test_inbox(p):
-    class a(Actor):
-        def act(self):
-            result = []
-            self.receive({
-                'answer': lambda msg: result.append(msg['answer'])})
-            return result[0]
-    x = a('a')
-    qt = ActorRef('p')
-    qt.send({'tag': 'foo'})
-    qt.send({'tag': 'bar'})
-    qt.send({'tag': 'queue',
-            'reply_to': 'a'})
-    assert x.act() == 2
-    x.close()
+# def test_inbox(p):
+#     class a(Actor):
+#         def act(self):
+#             result = []
+#             self.receive({
+#                 'answer': lambda msg: result.append(msg['answer'])})
+#             return result[0]
+#     x = a('a')
+#     qt = ActorRef('p')
+#     qt.send({'tag': 'foo'})
+#     qt.send({'tag': 'bar'})
+#     qt.send({'tag': 'queue',
+#             'reply_to': 'a'})
+#     assert x.act() == 2
+#     x.close()
 
-def test_non_existent_actor_ref():
-    x = ActorRef('foobar')
-    not_alive = not x.is_alive()
-    assert not_alive
-    not_alive = not x.is_alive()
-    assert not_alive
-    x.close()
+# def test_non_existent_actor_ref():
+#     x = ActorRef('foobar')
+#     not_alive = not x.is_alive()
+#     assert not_alive
+#     not_alive = not x.is_alive()
+#     assert not_alive
+#     x.close()
 
-def test_existent_actor_ref(t, p):
-    x = ActorRef('t')
-    alive = x.is_alive()
-    assert alive
-    y = ActorRef('p')
-    alive = y.is_alive()
-    assert alive
-    x.close()
-    y.close()
+# def test_existent_actor_ref(t, p):
+#     x = ActorRef('t')
+#     alive = x.is_alive()
+#     assert alive
+#     y = ActorRef('p')
+#     alive = y.is_alive()
+#     assert alive
+#     x.close()
+#     y.close()
 
-def test_timeout_zero():
-    class a(Actor):
-        def act(self):
-            self.receive({
-                'foo': self.read_value('data'),
-                }, timeout=0)
-            return getattr(self, 'data', None)
-    x = a()
-    ActorRef(x.name).send({'tag': 'foo', 'data': 1})
-    while x.act() is None:
-        time.sleep(0.1)
-    y = x.act()
-    assert y == 1
-    x.close()
+# def test_timeout_zero():
+#     class a(Actor):
+#         def act(self):
+#             self.receive({
+#                 'foo': self.read_value('data'),
+#                 }, timeout=0)
+#             return getattr(self, 'data', None)
+#     x = a()
+#     ActorRef(x.name).send({'tag': 'foo', 'data': 1})
+#     while x.act() is None:
+#         time.sleep(0.1)
+#     y = x.act()
+#     assert y == 1
+#     x.close()
 
-def test_timeout_zero_2():
-    class a(Actor):
-        def act(self):
-            self.receive({
-                'foo': self.read_value('data'),
-                }, timeout=0)
-            return getattr(self, 'data', None)
-    x = a()
-    y = ActorRef(x.name)
-    y.send({'tag': 'bar'})
-    y.send({'tag': 'baz'})
-    y.send({'tag': 'foo', 'data': 1})
-    y.send({'tag': 'gii'})
-    while x.inbox.qsize() < 4:
-        time.sleep(0.1)
-    z = x.act()
-    assert z == 1
-    x.close()
+# def test_timeout_zero_2():
+#     class a(Actor):
+#         def act(self):
+#             self.receive({
+#                 'foo': self.read_value('data'),
+#                 }, timeout=0)
+#             return getattr(self, 'data', None)
+#     x = a()
+#     y = ActorRef(x.name)
+#     y.send({'tag': 'bar'})
+#     y.send({'tag': 'baz'})
+#     y.send({'tag': 'foo', 'data': 1})
+#     y.send({'tag': 'gii'})
+#     while x.inbox.qsize() < 4:
+#         time.sleep(0.1)
+#     z = x.act()
+#     assert z == 1
+#     x.close()
         
-def test_timeout_zero_no_match():
-    class a(Actor):
-        def act(self):
-            self.receive({
-                'foo': self.read_value('data')
-                }, timeout=0)
-            return getattr(self, 'data', None)
-    x = a()
-    ActorRef(x.name).send({'tag': 'bar', 'data': 2})
-    while x.inbox.qsize() != 1:
-        time.sleep(0.1)
-    y = x.act()
-    y = x.act()
-    y = x.act()
-    y = x.act()
-    assert y == None
-    x.close()
+# def test_timeout_zero_no_match():
+#     class a(Actor):
+#         def act(self):
+#             self.receive({
+#                 'foo': self.read_value('data')
+#                 }, timeout=0)
+#             return getattr(self, 'data', None)
+#     x = a()
+#     ActorRef(x.name).send({'tag': 'bar', 'data': 2})
+#     while x.inbox.qsize() != 1:
+#         time.sleep(0.1)
+#     y = x.act()
+#     y = x.act()
+#     y = x.act()
+#     y = x.act()
+#     assert y == None
+#     x.close()
 
-def test_timeout_eating_msgs():
-    result = [True]
-    class a(Actor):
-        def act(self):
-            self.receive({}, timeout=0.1)
-        def act2(self):
-            self.receive(
-                bar = lambda msg: None,
-                timed_out = lambda msg: result.append(False),
-                timeout = 0.1)
-    x = a()
-    ActorRef(x.name).send({'tag': 'bar'})
-    while x.inbox.qsize() != 1:
-        time.sleep(0.1)
-    x.act()
-    x.act2()
-    assert result[-1]
-    x.close()
+# def test_timeout_eating_msgs():
+#     result = [True]
+#     class a(Actor):
+#         def act(self):
+#             self.receive({}, timeout=0.1)
+#         def act2(self):
+#             self.receive(
+#                 bar = lambda msg: None,
+#                 timed_out = lambda msg: result.append(False),
+#                 timeout = 0.1)
+#     x = a()
+#     ActorRef(x.name).send({'tag': 'bar'})
+#     while x.inbox.qsize() != 1:
+#         time.sleep(0.1)
+#     x.act()
+#     x.act2()
+#     assert result[-1]
+#     x.close()
 
-def test_process_actor_returns_name(q):
-    p, _ = spawn(q, 'foo')
-    ref = ActorRef('foo')
-    assert p == 'foo'
-    ActorRef('foo').close_actor()
+# def test_process_actor_returns_name(q):
+#     p, _ = spawn(q, 'foo')
+#     ref = ActorRef('foo')
+#     assert p == 'foo'
+#     ActorRef('foo').close_actor()
     
-def test_process_with_arg(q2):
-    class a(Actor):
-        def act(self):
-            self.receive(reply = self.read_value('x'))
-            return self.x
-    x = a()
-    spawn(q2, 'foo2', x=5)
-    ref = ActorRef('foo2')
-    ref.get_x(reply_to=x.name)
-    u = x.act()
-    assert u == 5
-    ref.close_actor()
-    x.close()
+# def test_process_with_arg(q2):
+#     class a(Actor):
+#         def act(self):
+#             self.receive(reply = self.read_value('x'))
+#             return self.x
+#     x = a()
+#     spawn(q2, 'foo2', x=5)
+#     ref = ActorRef('foo2')
+#     ref.get_x(reply_to=x.name)
+#     u = x.act()
+#     assert u == 5
+#     ref.close_actor()
+#     x.close()
 
-def test_close_with_confirmation(t):
-    class a(Actor):
-        def act(self):
-            self.receive(_ = self.read_value('tag'))
-            return self.tag
-    x = a()
-    with ActorRef('t') as tr:
-        alive = tr.is_alive()
-        assert alive
-        tr.close_actor(confirm_to=x.name)
-        u = x.act()
-        assert u == 'closed'
-    x.close()
+# def test_close_with_confirmation(t):
+#     class a(Actor):
+#         def act(self):
+#             self.receive(_ = self.read_value('tag'))
+#             return self.tag
+#     x = a()
+#     with ActorRef('t') as tr:
+#         alive = tr.is_alive()
+#         assert alive
+#         tr.close_actor(confirm_to=x.name)
+#         u = x.act()
+#         assert u == 'closed'
+#     x.close()
 
-def test_ping():
-    class a(ThreadedActor):
-        def act(self):
-            try:
-                self.receive(foo=lambda msg: None)
-            except ActorFinished:
-                pass
-    x = a()
-    xr = ActorRef(x.name)
-    alive = xr.is_alive()
-    assert alive
-    time.sleep(0.5)
-    alive = xr.is_alive()
-    assert alive
-    x.close()
+# def test_ping():
+#     class a(ThreadedActor):
+#         def act(self):
+#             try:
+#                 self.receive(foo=lambda msg: None)
+#             except ActorFinished:
+#                 pass
+#     x = a()
+#     xr = ActorRef(x.name)
+#     alive = xr.is_alive()
+#     assert alive
+#     time.sleep(0.5)
+#     alive = xr.is_alive()
+#     assert alive
+#     x.close()
 
-def test_none_method():
-    class a(Actor):
-        def act(self):
-            self.receive(foo=None)
-            return True
-    x = a()
-    xr = ActorRef(x.name)
-    xr.foo()
-    y = x.act()
-    assert y
-    x.close()
+# def test_none_method():
+#     class a(Actor):
+#         def act(self):
+#             self.receive(foo=None)
+#             return True
+#     x = a()
+#     xr = ActorRef(x.name)
+#     xr.foo()
+#     y = x.act()
+#     assert y
+#     x.close()
 
-def test_close_actor_and_ref():
-    class Wait(Actor):
-        def act(self):
-            self.receive(_=None)
-    class a(ThreadedActor):
-        def act(self):
-            self.receive()
-    x = a()
-    wait = Wait()
-    xr = ActorRef(x.name)
-    alive = xr.is_alive()
-    assert alive
-    xr.close_actor(confirm_to=wait.name)
-    wait.act()
-    not_alive = not xr.is_alive()
-    assert not_alive
-    x.close()
-    wait.close()
+# def test_close_actor_and_ref():
+#     class Wait(Actor):
+#         def act(self):
+#             self.receive(_=None)
+#     class a(ThreadedActor):
+#         def act(self):
+#             self.receive()
+#     x = a()
+#     wait = Wait()
+#     xr = ActorRef(x.name)
+#     alive = xr.is_alive()
+#     assert alive
+#     xr.close_actor(confirm_to=wait.name)
+#     wait.act()
+#     not_alive = not xr.is_alive()
+#     assert not_alive
+#     x.close()
+#     wait.close()
 
-def test_sync_call(t):
-    class a(ThreadedActor):
-        def act(self):
-            self.receive(sync_test=self.sync_test)
-        def sync_test(self, msg):
-            with ActorRef(msg['reply_to']) as sender:
-                sender.reply(got=msg)
-    x = a()
-    with ActorRef(x.name) as tr:
-        assert tr.is_alive()
-        answer = tr.sync('sync_test', x=5)
-        assert answer['got']['tag'] == 'sync_test'
-        assert answer['got']['x'] == 5
-    x.close()
+# def test_sync_call(t):
+#     class a(ThreadedActor):
+#         def act(self):
+#             self.receive(sync_test=self.sync_test)
+#         def sync_test(self, msg):
+#             with ActorRef(msg['reply_to']) as sender:
+#                 sender.reply(got=msg)
+#     x = a()
+#     with ActorRef(x.name) as tr:
+#         assert tr.is_alive()
+#         answer = tr.sync('sync_test', x=5)
+#         assert answer['got']['tag'] == 'sync_test'
+#         assert answer['got']['x'] == 5
+#     x.close()
 
-def test_alive_not_acting():
-    class a(Actor):
-        def act(self):
-            self.receive(_=self.read_value('tag'))
-            return self.tag
-    x = a()
-    xr = ActorRef(x.name)
-    alive = xr.is_alive()
-    assert alive
-    xr.foo()
-    u = x.act()
-    assert u == 'foo'
+# def test_alive_not_acting():
+#     class a(Actor):
+#         def act(self):
+#             self.receive(_=self.read_value('tag'))
+#             return self.tag
+#     x = a()
+#     xr = ActorRef(x.name)
+#     alive = xr.is_alive()
+#     assert alive
+#     xr.foo()
+#     u = x.act()
+#     assert u == 'foo'
