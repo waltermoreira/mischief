0d07bd23353ad52a1d28ff2e0a215c7c8832d09d
Walter Moreira <walter@waltermoreira.net>
2013-11-22 20:22:09 -0600

    Rewrite ping_pong with new actor code

--- mischief/actors/ping_pong.py
+++ mischief/actors/ping_pong.py
@@ -1,58 +1,52 @@
-from het2_common.actors.actor import ThreadedActor, ActorRef
-from het2_common.actors.process_actor import ProcessActor, spawn
 import time
 
-class Ping(ProcessActor):
+from mischief.actors.actor import ActorRef, spawn, ThreadedActor
+from mischief.actors.process_actor import ProcessActor
 
-    def __init__(self):
-        self.c = 0
-        super(Ping, self).__init__('ping')
+ActorKind = ProcessActor
 
-    def process_act(self):
-        if self.c >= 1000:
-            self.receive(start_time=self.get_start_time)
-        else:
-            self.receive(ping=self.ping,
-                         set_time=self.set_time)
+class Ping(ActorKind):
+
+    def act(self):
+        while True:
+            if self.c >= 1000:
+                self.receive(start_time=self.get_start_time)
+            else:
+                self.receive(ping=self.ping,
+                             set_time=self.set_time)
 
     def get_start_time(self, msg):
-        print self.starting_time
+        print('Ping starting time:', self.starting_time)
             
     def set_time(self, msg):
         self.starting_time = time.time()
 
     def ping(self, msg):
-        print 'ping', self.c
+        print('ping', self.c)
         self.c += 1
-        with ActorRef(msg['reply_to']) as sender:
-            sender.pong(reply_to=self.name)
+        with ActorRef(msg.reply_to) as sender:
+            sender.pong(reply_to=self)
 
-class Pong(ProcessActor):
+class Pong(ActorKind):
 
-    def __init__(self):
-        self.c = 0
-        super(Pong, self).__init__('pong')
+    def act(self):
+        while True:
+            if self.c >= 999:
+                print('Pong end time:', time.time())
+            self.receive(pong=self.pong)
 
-    def process_act(self):
-        if self.c >= 999:
-            print time.time()
-        self.receive(pong=self.pong)
-
-    def get_end_time(self, msg):
-        print time.time()
-        
     def pong(self, msg):
-        print 'pong', self.c
+        print('pong', self.c)
         self.c += 1
-        with ActorRef(msg['reply_to']) as sender:
-            sender.ping(reply_to=self.name)
+        with ActorRef(msg.reply_to) as sender:
+            sender.ping(reply_to=self)
 
 
 def run():
-    ping, _ = spawn(Ping, 'ping', c=0)
-    pong, _ = spawn(Pong, 'pong', c=0)
-    print 'Starting'
-    with ActorRef('ping') as ping_ref:
+    ping = spawn(Ping, c=0)
+    pong = spawn(Pong, c=0)
+    print('Starting')
+    with ActorRef(ping) as ping_ref:
         ping_ref.set_time()
-        ping_ref.ping(reply_to='pong')
+        ping_ref.ping(reply_to=pong)
     return ping, pong
